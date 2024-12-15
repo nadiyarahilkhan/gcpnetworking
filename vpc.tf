4d22e0469a651d811d857f78079f82bd9b0b85a5
testlab <testmail@ow.com>
2024-12-15 10:49:14 +0530

    updating the vpc file with subnet block

--- vpc.tf
+++ vpc.tf
@@ -1,5 +1,13 @@
 resource "google_compute_network" "vpc_network" {
-  project                                   = "apt-memento-442605-f0"
-  name                                      = "nadia-vpc"
-  auto_create_subnetworks                   = false
+  project                  = "apt-memento-442605-f0" 
+  name                     = "nadia-vpc"            
+  auto_create_subnetworks  = false                  
+}
+
+resource "google_compute_subnetwork" "subnet_1" {
+  name          = "nadia-subnet-1"                  
+  ip_cidr_range = "10.0.0.0/24"                     
+  region        = "us-central1"                     
+  network       = google_compute_network.vpc_network.self_link 
+  description   = "Subnet 1 - for VPC"
 }
